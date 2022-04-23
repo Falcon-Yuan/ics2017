9320884ee7aad8c8eb3399bdc92d636d7717ab3a
tracer-ics2017 <tracer@njuics.org>
2022-04-23 20:34:27 +0800

    >  compile 1911509 falcon Linux vm 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:34:27 up  1:39,  1 user,  load average: 1.33, 0.94, 0.57 bc22ecfa1ef3eb58c88a71464bf5c3e380017fd6

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,23 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  	// 通过is_mmio () 函数判断一个物理地址是否被映射到I/O 空间
+	int id = is_mmio(addr);
+	
+	if (id == -1) // 没有映射到I/O 空间
+		return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+	// 映射到I/O 空间
+	return mmio_read(addr, len, id);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int id = is_mmio(addr);
+	
+	if (id == -1) // 没有映射到I/O 空间
+		memcpy(guest_to_host(addr), &data, len);
+	else // 映射到I/O 空间
+		mmio_write(addr, len, data, id);
+
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
