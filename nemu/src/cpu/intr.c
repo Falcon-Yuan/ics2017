3ffb604182d45c6e138e0218262d0f52ba7d45a4
tracer-ics2017 <tracer@njuics.org>
2022-05-15 14:48:58 +0800

    >  compile 1911509 falcon Linux vm 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  14:48:58 up 11:33,  1 user,  load average: 0.45, 0.34, 0.23 cffc593c6a8a50caa6e0b5116fc8d3451e12289c

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,18 +5,22 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  rtl_push((rtlreg_t*)&cpu.eflags);
-  rtl_push((rtlreg_t*)&cpu.cs);
-  rtl_push((rtlreg_t*)&ret_addr);
-  uint32_t idtr_base = cpu.idtr.base;
-  // 下一步执行什么
-  uint32_t eip_low, eip_high, offset;
-  eip_low = vaddr_read(idtr_base + NO * 8, 4) & 0x0000ffff;
-  eip_high = vaddr_read(idtr_base + NO * 8 + 4, 4) & 0xffff0000;
-  offset = eip_low | eip_high;
-  decoding.jmp_eip = offset;
-  decoding.is_jmp = true;
+  memcpy(&t1,&cpu.eflags,sizeof(cpu.eflags));
+  rtl_li(&t0,t1);
+	rtl_push(&t0);
+	rtl_push(&cpu.cs);
+  rtl_li(&t0,ret_addr);
+	rtl_push(&t0);
 
+  vaddr_t gate_addr=cpu.idtr.base + NO * sizeof(GateDesc);
+  assert(gate_addr <= cpu.idtr.base + cpu.idtr.limit);
+
+	uint32_t eip_low, eip_high, offset;
+	eip_low = vaddr_read(gate_addr,2);
+	eip_high = vaddr_read(gate_addr + sizeof(GateDesc) - 2,2);
+	offset = (eip_high << 16) + eip_low;
+	decoding.jmp_eip = offset;
+	decoding.is_jmp = true;
 }
 
 void dev_raise_intr() {
