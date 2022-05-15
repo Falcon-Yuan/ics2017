99b3b102e7943c0f67b9935d964733f82d1f0a81
tracer-ics2017 <tracer@njuics.org>
2022-05-15 16:09:02 +0800

    >  run 1911509 falcon Linux vm 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:09:02 up 12:53,  1 user,  load average: 2.08, 2.16, 2.12 e7f50c6fae41cec79810f0d02e084e334d6578ab

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,19 +12,17 @@ int sys_none(){
 }
 
 int sys_write(int fd,void* buf,size_t len) {
-	int i = 0;
-	if (fd == 1 || fd == 2) {
-    Log("buffer:%s",(char*)buf);
-		for(; len > 0; len--) {
-			_putc(((char*)buf)[i]);
-			i++;;
-		}
-	}
-  else {
-    panic("Unhandled fd = %d in sys_write",fd);
-    return -1;
+  if(fd ==1 || fd ==2) {
+    char c;
+    for(int i=0;i<len;++i){
+      memcpy(&c,buf+i,1);
+      _putc(c);
+    }
+    return len;
   }
-	return i;
+  else
+    panic("Unhandled fd = %d in sys_write",fd);
+  return -1;
 }
 
 int sys_brk(int addr){
