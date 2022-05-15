b2a3a37c3529c3e99384c7e6bd54aa920957eaff
tracer-ics2017 <tracer@njuics.org>
2022-05-15 18:45:46 +0800

    >  run 1911509 falcon Linux vm 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:45:46 up 13:34,  1 user,  load average: 0.10, 0.19, 0.27 5c2382cc27082843c045e062bb8453731a4d3d84

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,11 +1,16 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  size_t size = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY, 0, size);
+  //size_t size = get_ramdisk_size();
+  //ramdisk_read(DEFAULT_ENTRY, 0, size);
+  int fd = fs_open(filename,0,0);
+  Log("filename = %s,fd = %d",filename,fd);
+  fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
