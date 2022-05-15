526dc1a0395f76dff8c16c427e5e73d0254a9287
tracer-ics2017 <tracer@njuics.org>
2022-05-15 15:57:31 +0800

    >  run 1911509 falcon Linux vm 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:57:31 up 12:42,  1 user,  load average: 2.41, 2.23, 2.08 12e228da25e5457ad369b9267705fba04b18b45a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,10 +7,25 @@ void sys_exit(int a){
   _halt(a);
 }
 
-
 int sys_none(){
   return 1;
 }
+
+int sys_write(int fd,void* buf,size_t len) {
+	int i = 0;
+	if (fd == 1 || fd == 2) {
+		for(; len > 0; len--) {
+			_putc(((char*)buf)[i]);
+			i++;;
+		}
+	}
+  else {
+    panic("Unhandled fd = %d in sys_write",fd);
+    return -1;
+  }
+	return i;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -19,8 +34,15 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-    case SYS_none:SYSCALL_ARG1(r)=sys_none();break;
-    case SYS_exit:sys_exit(a[1]);break;
+    case SYS_none:
+      SYSCALL_ARG1(r)=sys_none();
+      break;
+    case SYS_exit:
+      sys_exit(a[1]);
+      break;
+    case SYS_write:
+      SYSCALL_ARG1(r) = sys_write(a[1],(void *)a[2],a[3]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
