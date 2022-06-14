52cd782e49973f68086cde1116a0f92586fe6cf9
tracer-ics2017 <tracer@njuics.org>
2022-06-14 22:52:27 +0800

    >  compile 1911509 falcon Linux vm 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  22:52:27 up 1 day, 16:30,  1 user,  load average: 0.67, 0.42, 0.54 6ecb12165b06f9ee1cb97d4faa9c716d4914592b

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -1,18 +1,48 @@
 #include "FLOAT.h"
 #include <stdint.h>
 #include <assert.h>
-
-FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+struct float_
+{
+  uint32_t frac : 23;
+  uint32_t exp : 8;
+  uint32_t sign : 1;
+};
+FLOAT F_mul_F(FLOAT a, FLOAT b)
+{
+  // assert(0);
+  // return 0;
+  return ((int64_t)a * (int64_t)b) >> 16;
 }
 
-FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+FLOAT F_div_F(FLOAT a, FLOAT b)
+{
+  // assert(0);
+  // return 0;
+  assert(b != 0);
+  FLOAT x = Fabs(a);
+  FLOAT y = Fabs(b);
+  FLOAT z = x / y;
+  x = x % y;
+
+  for (int i = 0; i < 16; i++)
+  {
+    x <<= 1;
+    z <<= 1;
+    if (x >= y)
+    {
+      x -= y;
+      z++;
+    }
+  }
+  if (((a ^ b) & 0x80000000) == 0x80000000)
+  {
+    z = -z;
+  }
+  return z;
 }
 
-FLOAT f2F(float a) {
+FLOAT f2F(float a)
+{
   /* You should figure out how to convert `a' into FLOAT without
    * introducing x87 floating point instructions. Else you can
    * not run this code in NEMU before implementing x87 floating
@@ -23,37 +53,62 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  assert(0);
-  return 0;
+  struct float_ *f = (struct float_ *)&a;
+  uint32_t res;
+  uint32_t frac;
+  int exp;
+  if ((f->exp & 0xff) == 0xff)
+    assert(0);
+  else if (f->exp == 0)
+  {
+    exp = 1 - 127;
+    frac = (f->frac & 0x7fffff);
+  }
+  else
+  {
+    exp = f->exp - 127;
+    frac = (f->frac & 0x7fffff) | (1 << 23);
+  }
+  if (exp >= 7 && exp < 22)
+    res = frac << (exp - 7);
+  else if (exp < 7 && exp > -32)
+    res = frac >> 7 >> -exp;
+  else
+    assert(0);
+  return (f->sign) ? -res : res;
 }
 
-FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+FLOAT Fabs(FLOAT a)
+{
+  return (a > 0) ? a : -a;
 }
 
 /* Functions below are already implemented */
 
-FLOAT Fsqrt(FLOAT x) {
+FLOAT Fsqrt(FLOAT x)
+{
   FLOAT dt, t = int2F(2);
 
-  do {
+  do
+  {
     dt = F_div_int((F_div_F(x, t) - t), 2);
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4));
 
   return t;
 }
 
-FLOAT Fpow(FLOAT x, FLOAT y) {
+FLOAT Fpow(FLOAT x, FLOAT y)
+{
   /* we only compute x^0.333 */
   FLOAT t2, dt, t = int2F(2);
 
-  do {
+  do
+  {
     t2 = F_mul_F(t, t);
     dt = (F_div_F(x, t2) - t) / 3;
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4));
 
   return t;
 }
